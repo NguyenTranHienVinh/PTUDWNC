2f01982b847d741137bb00a988320f642f973a22
VinhNTH <nguyentranhienvinh@gmail.com>
2023-02-26 18:51:47 +0700

    first commit

---
+++ TipsAndTricks/TatBlog.Core/TatBlog.Core/Entities/Category.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TatBlog.Core.Contracts;
+
+namespace TatBlog.Core.Entities;
+
+public class Category : IEntity
+{
+    //mã chuyên mục
+    public int id { get; set; }
+    //tên chuyên mục
+    public string name { get; set; }
+    //định danh dùng để tạo url
+    public string UrlSlug { get; set; }
+    //mô tả về chuyên mục
+    public int Description { get; set; }
+    //đánh dấu chuyên mục được hiển thị trên menu 
+    public bool ShowOnMenu { get; set; }
+}
